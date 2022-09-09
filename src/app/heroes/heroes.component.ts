3e8a28b555ced5e96adfe467ed2849b164830f84
BrandonCarlos <brandon_marzo@hotmail.com>
2022-09-09 11:52:07 -0300

    Import FormsModule in app.module.ts to control input using concept of two-ways-binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
 
+
 @Component({
   selector: 'app-heroes', //<app-heroes></app-heroes>, colocamos esse componente lá no arquivo "app.component.html"
   templateUrl: './heroes.component.html',
@@ -15,6 +16,7 @@ export class HeroesComponent implements OnInit {//export para usar em outros lug
   constructor() { }
 
   ngOnInit(): void {//here is good place to put logic
+
   }
 
 }
